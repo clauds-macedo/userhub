11cb98f925f9109abc9869aefe286c613254cd7a
clauds-macedo <yanalogic@gmail.com>
2024-09-02 21:22:56 -0300

    feat: create mocked user repository

---
+++ src/__tests__/__mocks__/MockUserRepository.ts
@@ -0,0 +1,50 @@
+import { ICreateUserDTO } from '@/domain/dtos/IUserDTO';
+import { IUser, User } from '@/domain/entities/User';
+import { IUserRepository } from '@/domain/repositories/IUserRepository';
+
+export class InMemoryUserRepository implements IUserRepository {
+  private users: User[] = [];
+  private nextId = 1;
+
+  async create(userDTO: ICreateUserDTO): Promise<User> {
+    const user = new User({
+      ...userDTO,
+      createdAt: new Date(),
+      id: this.nextId++,
+      addresses: [],
+    });
+    this.users.push(user);
+    return user;
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.users.find((user) => user.id === id) || null;
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.users;
+  }
+
+  async update(id: number, userDTO: Partial<IUser>): Promise<User | null> {
+    const user = await this.findById(id);
+    if (!user) {
+      return null;
+    }
+    if (userDTO.name !== undefined) user.name = userDTO.name;
+    if (userDTO.email !== undefined) user.email = userDTO.email;
+    if (userDTO.password !== undefined) user.password = userDTO.password;
+
+    return user;
+  }
+
+  async remove(id: number): Promise<boolean> {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) return false;
+    this.users.splice(index, 1);
+    return true;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.users.find((user) => user.email === email) || null;
+  }
+}
