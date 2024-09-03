8faf02321f099c045b82bd95aa2b31ed38f98514
clauds-macedo <yanalogic@gmail.com>
2024-09-03 07:34:54 -0300

    feat: add remaining operators on mongoose user repo

--- src/infra/repositories/MongooseUserRepository.ts
+++ src/infra/repositories/MongooseUserRepository.ts
@@ -5,11 +5,6 @@ import { UserMapper } from '../mongoose/mappers/UserMapper';
 import { MongooseUser } from '../mongoose/models/MongooseUser';
 
 export class MongooseUserRepository implements IUserRepository {
-  remove: (id: string) => Promise<boolean>;
-  update: (id: string, user: Partial<IUser>) => Promise<User | null>;
-  findById: (id: string) => Promise<User | null>;
-  findAll: () => Promise<User[]>;
-
   async create(user: ICreateUserDTO): Promise<User> {
     const userDocument = new MongooseUser(UserMapper.toPersistence(user));
     await userDocument.save();
@@ -17,6 +12,48 @@ export class MongooseUserRepository implements IUserRepository {
     return UserMapper.toDomain({
       id: _id.toString(),
       ...rest,
+    });
+  }
+
+  async remove(id: string): Promise<boolean> {
+    const result = await MongooseUser.deleteOne({ _id: id }).exec();
+    return result.deletedCount > 0;
+  }
+
+  async update(id: string, user: Partial<IUser>): Promise<User | null> {
+    const userDocument = await MongooseUser.findByIdAndUpdate(id, user, {
+      new: true,
+    }).exec();
+    if (!userDocument) {
+      return null;
+    }
+    const { _id, ...rest } = userDocument.toObject();
+    return UserMapper.toDomain({
+      id: _id.toString(),
+      ...rest,
+    });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const userDocument = await MongooseUser.findById(id).exec();
+    if (!userDocument) {
+      return null;
+    }
+    const { _id, ...rest } = userDocument.toObject();
+    return UserMapper.toDomain({
+      id: _id.toString(),
+      ...rest,
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    const userDocuments = await MongooseUser.find().exec();
+    return userDocuments.map((userDocument) => {
+      const { _id, ...rest } = userDocument.toObject();
+      return UserMapper.toDomain({
+        id: _id.toString(),
+        ...rest,
+      });
     });
   }
 }
